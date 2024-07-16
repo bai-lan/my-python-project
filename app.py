dc4b87e645fdbdfb3232419afce933d8544a37df
bailan <bai-lan@outlook.com>
2024-07-16 11:20:19 +0800

    this is app version 2

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return jsonify({"message": "Welcome to the API!"}), 200
-
-@app.route('/about')
-def about():
-    return jsonify({"version": "1.0", "author": "Bai Lan"}), 200
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run(host='0.0.0.0', port=5000)
